efa4d26ea6b6dbb8e0d76a1efdf771f99abca7d5
Eduard-Sound <eduard.barba@enti.cat>
2024-12-06 00:31:07 +0100

    FootSteps and Music

--- LeafBlower/Assets/Scripts/Characters/Player/PlayerSounds.cs
+++ LeafBlower/Assets/Scripts/Characters/Player/PlayerSounds.cs
@@ -3,5 +3,16 @@ using UnityEngine;
 
 public class PlayerSounds : MonoBehaviour
 {
-    public void PlayFootSteps() => RuntimeManager.PlayOneShot("event:/Character/FootSteps/FootSteps_Concrete", transform.position);
+    private PlayerController _player;
+
+    private void Awake()
+    {
+        _player = GetComponent<PlayerController>();
+    }
+    public void PlayFootSteps()
+    {
+        if (_player.CheckCollisions.IsGrounded && _player.CurrentCharacterState != Enums.CharacterState.Idle)
+            RuntimeManager.PlayOneShot("event:/Character/FootSteps/FootSteps_Concrete", transform.position);
+
+    }
 }
