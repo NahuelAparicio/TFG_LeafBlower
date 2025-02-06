8e482574bc7bc6fdba9811933e111e1adf1c7be9
NahuelAparicio <nahuel.aparicio@enti.cat>
2025-02-06 13:48:08 +0100

    Transparent Shader Fixed

--- LeafBlower/Assets/Scripts/Objects/TransparentObject.cs
+++ LeafBlower/Assets/Scripts/Objects/TransparentObject.cs
@@ -1,80 +1,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransparentObject : MonoBehaviour, ITransparencyHandler
+public class TransparentObject : MonoBehaviour
 {
     public static int PosID = Shader.PropertyToID("_PlayerPos");
-    public static int SizeID = Shader.PropertyToID("_Size");
+    public static int IsTransparentID = Shader.PropertyToID("_IsTransparent");
 
-    public bool isTransparent = false;
-    [SerializeField] private List<Material> materials = new List<Material>();
+    private Material _material;
     private Transform _player;
-    public float transparencyRadius = 5;
+    private bool _isTransparent = false;
     private void Awake()
     {
         _player = FindObjectOfType<PlayerController>().transform;
+        _material = GetComponent<Renderer>().material;
     }
 
     private void Update()
     {
-
         var dir = _player.position - Camera.main.transform.position;
         var ray = new Ray(Camera.main.transform.position, dir);
-        if(Physics.Raycast(ray, 3000))
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, dir.magnitude))
         {
-            foreach (Material mat in materials)
+            if(hit.collider.gameObject == gameObject)
             {
-                mat.SetFloat(SizeID, 1);
+                if(!_isTransparent)
+                {
+                    SetTransparency(true);
+                }
+            }
+            else
+            {
+                if(_isTransparent)
+                {
+                    SetTransparency(false);
+                }
             }
         }
-        else
-        {
-            foreach (Material mat in materials)
-            {
-                mat.SetFloat(SizeID, 0);
-            }
-        }
+        
+        if (!_isTransparent) return;
+
+        SetPlayerPos();
+
+    }
+
+    private void SetTransparency(bool makeTransparent)
+    {
+        _material.SetInt("_IsTransparent", makeTransparent ? 1 : 0);
+        _isTransparent = makeTransparent;
+    }
+
+    private void SetPlayerPos()
+    {
         var view = Camera.main.WorldToViewportPoint(_player.position);
-        foreach (Material mat in materials)
-        {
-            mat.SetVector(PosID, view);
-        }
-
-    }
-    public void DisableTransparent()
-    {
-        //if (!isTransparent) return;
-
-        //foreach (Material mat in materials)
-        //{
-        //    Color color = mat.color;
-        //    color.a = 1.0f; // Opacidad (completamente opaco)
-        //    mat.color = color;
-
-        //    // Restaura el material a opaco
-        //    //mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-        //    //mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-        //    //mat.SetInt("_ZWrite", 1);
-        //    //mat.DisableKeyword("_ALPHABLEND_ON");
-        //    //mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Geometry;
-        //}
-    }
-
-    public void EnableTransparent()
-    {
-        //if (isTransparent) return;
-        //isTransparent = true;
-        //foreach (Material mat in materials)
-        //{
-        //    Color color = mat.color;
-        //    color.a = 0.5f; // Opacidad (50%)
-        //    mat.color = color;
-        //    // Asegura que el material sea transparente
-        //    //mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-        //    //mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-        //    //mat.SetInt("_ZWrite", 0);
-        //    //mat.EnableKeyword("_ALPHABLEND_ON");
-        //    //mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
-        //}
+        _material.SetVector(PosID, view);
     }
 }
