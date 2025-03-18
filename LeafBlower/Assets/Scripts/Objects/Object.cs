beffc24116706b995af96c5a2adf9a3c540e3284
NahuelAparicio <nahuel.aparicio@enti.cat>
2025-03-18 13:34:41 +0100

    To test Optimzatin

--- LeafBlower/Assets/Scripts/Objects/Object.cs
+++ LeafBlower/Assets/Scripts/Objects/Object.cs
@@ -26,6 +26,7 @@ public class Object : MonoBehaviour
 
     protected virtual void Update()
     {
+        if (_rb.velocity.magnitude < 0.15f) return;
         if(Time.frameCount % 70 == 0)
         {
             if(transform.position.y <=  -35)
