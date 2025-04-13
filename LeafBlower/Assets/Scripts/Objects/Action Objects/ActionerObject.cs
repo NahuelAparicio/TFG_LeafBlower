8b424a496af0bd1851bb9d09b0675ab93881104d
Eduard-Sound <eduard.barba@enti.cat>
2025-04-13 20:47:41 +0200

    Sonidos
    Muro, contenedores, sound fixing + laca

--- LeafBlower/Assets/Scripts/Objects/Action Objects/ActionerObject.cs
+++ LeafBlower/Assets/Scripts/Objects/Action Objects/ActionerObject.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.Events;
+using FMODUnity;
+using FMOD.Studio;
 
 public class ActionerObject : NormalObject
 {
     private bool _hasBeenActived = false;
-    public UnityEvent OnAction; 
+    public UnityEvent OnAction;
+
+    [Header("FMOD")]
+    public EventReference fmodEvent;
 
     protected override void Awake()
     {
@@ -14,10 +19,27 @@ public class ActionerObject : NormalObject
 
     public override void OnBlow(Vector3 force, Vector3 point)
     {
-        if(!_hasBeenActived)
+        if (!_hasBeenActived)
         {
+            PlayFmodEvent();
             OnAction?.Invoke();
             _hasBeenActived = true;
         }
     }
+
+    private void PlayFmodEvent()
+    {
+        if (fmodEvent.IsNull)
+        {
+            Debug.LogWarning("FMOD event no asignado en " + gameObject.name);
+            return;
+        }
+
+        var instance = RuntimeManager.CreateInstance(fmodEvent);
+        RuntimeManager.AttachInstanceToGameObject(instance, transform, GetComponent<Rigidbody>());
+        instance.start();
+        instance.release();
+    }
 }
+
+
